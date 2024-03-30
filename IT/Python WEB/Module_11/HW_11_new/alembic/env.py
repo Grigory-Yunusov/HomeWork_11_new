d71677d5d5ed4d85d18f89bd1496c9a5eb33893f
Grigory-Yunusov <grigoriyzawr@gmail.com>
2024-03-30 23:11:07 +0300

    problem with import models

--- IT/Python WEB/Module_11/HW_11_new/alembic/env.py
+++ IT/Python WEB/Module_11/HW_11_new/alembic/env.py
@@ -5,9 +5,12 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from src.models.user import UserDB
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+target_metadata = UserDB.metadata
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
